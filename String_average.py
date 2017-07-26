da29cc781a57988b027969313c77786c636a35b9
Katia Punter <katiapunter@gmail.com>
2017-07-26 20:53:54 +0100

    String average added

---
+++ String_average.py
@@ -0,0 +1,57 @@
+def numToWords(num,join=True):
+    '''words = {} convert an integer number into words'''
+    units = ['','one','two','three','four','five','six','seven','eight','nine']
+    teens = ['','eleven','twelve','thirteen','fourteen','fifteen','sixteen', \
+             'seventeen','eighteen','nineteen']
+    tens = ['','ten','twenty','thirty','forty','fifty','sixty','seventy', \
+            'eighty','ninety']
+    thousands = ['','thousand','million','billion','trillion','quadrillion', \
+                 'quintillion','sextillion','septillion','octillion', \
+                 'nonillion','decillion','undecillion','duodecillion', \
+                 'tredecillion','quattuordecillion','sexdecillion', \
+                 'septendecillion','octodecillion','novemdecillion', \
+                 'vigintillion']
+    words = []
+    if num==0: words.append('zero')
+    else:
+        numStr = '%d'%num
+        numStrLen = len(numStr)
+        groups = (numStrLen+2)//3
+        numStr = numStr.zfill(groups*3)
+        for i in range(0,groups*3,3):
+            h,t,u = int(numStr[i]),int(numStr[i+1]),int(numStr[i+2])
+            g = groups-(i/3+1)
+            if h>=1:
+                words.append(units[h])
+                words.append('hundred')
+            if t>1:
+                words.append(tens[t])
+                if u>=1: words.append(units[u])
+            elif t==1:
+                if u>=1: words.append(teens[u])
+                else: words.append(tens[t])
+            else:
+                if u>=1: words.append(units[u])
+            if (g>=1) and ((h+t+u)>0): words.append(thousands[g]+',')
+    if join: return ' '.join(words)
+    return words
+
+def average_string(s):
+    if s=="":
+        return "n/a"
+    dictNum = {"zero":0,"one":1,"two":2,"three":3,"four":4, "five":5,"six":6,"seven":7,"eight":8,"nine":9}
+    lst = s.split()
+    lstNum=[]
+    ave=0
+    for i in lst:
+        if i in dictNum:
+            lstNum.append(dictNum[i])
+           
+        else:
+            return "n/a"
+
+    ave = sum(lstNum) // len(lstNum)
+        
+        
+    
+    return numToWords(ave)
